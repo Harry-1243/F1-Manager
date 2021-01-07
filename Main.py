94be1aa12cec08baf0d2a05f5be29ce54c386137
Harry-1243 <74413940+Harry-1243@users.noreply.github.com>
2021-01-07 15:30:22 +0000

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,6 +1,59 @@
 import random
 import time
 laplength = 6
+car02bpitted = 0
+car12bpitted = 0
+currenttyre1 = ""
+currenttyre0 = ""
+tobtyre1 = ""
+tobtyre0 = ""
+car1lapspeed = []
+car0lapspeed = []
+HAMlapspeed = []
+BOTlapspeed = []
+VERlapspeed = []
+ALBlapspeed = []
+NORlapspeed = []
+RIClapspeed = []
+OCOlapspeed = []
+STRlapspeed = []
+PERlapspeed = []
+GROlapspeed = []
+MAGlapspeed = []
+RUSlapspeed = []
+KVYlapspeed = []
+PIElapspeed = []
+LEClapspeed = []
+VETlapspeed = []
+RAIlapspeed = []
+GIOlapspeed = []
+sectorgaptime0 = []
+sectorgaptime1 = []
+sectorgaptimeHAM = []
+sectorgaptimeBOT = []
+sectorgaptimeVER = []
+sectorgaptimeALB = []
+sectorgaptimeNOR = []
+sectorgaptimeRIC = []
+sectorgaptimeOCO = []
+sectorgaptimeSTR = []
+sectorgaptimePER = []
+sectorgaptimeGRO = []
+sectorgaptimeMAG = []
+sectorgaptimeRUS = []
+sectorgaptimeKVY = []
+sectorgaptimePIE = []
+sectorgaptimeLEC = []
+sectorgaptimeVET = []
+sectorgaptimeRAI = []
+sectorgaptimeGIO = []
+
+def pitlane1(Pstime, Tyredelta):
+    Pstime = random.randint(18.9, 21.9)
+    if tobtyre1 == "Medium":
+        
+
+
 print("Please enter settings for car 1")
 
 # user inputted car settings
@@ -25,9 +78,8 @@ OAPCircuit = [330, 175, 315, 110, 185, 270, 155, 275, 300, 130, 255, 310, 265, 3
 OAPCircuitG = [0, 8, 4, 8, 3, 4, 7, 4, 7, 7, 3, 6, 8, 7, 7, 8, 4, 6, 5, 2, 4]
 
 # Determining Car speeds in race
-car0lapspeed = []
-car1lapspeed = []
-for x in range(len(OAPCircuit)):
+
+for x in range(len(OAPCircuit[x])):
     Randspeed0 = int(random.randint(1, 100))
     Randposneg0 = int(random.randint(0, 100))
     if Randposneg0 % 2 == 0:
@@ -40,19 +92,30 @@ for x in range(len(OAPCircuit)):
         car1lapspeed.append((OAPCircuit[x] + Randspeed1))
     else:
         car1lapspeed.append((OAPCircuit[x] - Randspeed1))
-# increasing or decreasing speed depending on the level of aro selected (car0)
+# increasing or decreasing speed depending on the level of aro selected
 plusminushigh = [-1, -1, 1, 1, 1, 1, -1, 1, 1, -1, 1, -1, 1, 1, 1, -1, 1, -1, 1, 1]
 plusminuslow = [1, 1, -1, -1, -1, -1, 1, -1, -1, 1, -1, 1, -1, -1, -1, 1, -1, 1, -1, -1]
 
+botracers = [HAMlapspeed, BOTlapspeed, VERlapspeed, ALBlapspeed, NORlapspeed, RIClapspeed, OCOlapspeed, STRlapspeed, PERlapspeed, GROlapspeed, MAGlapspeed, RUSlapspeed, KVYlapspeed, PIElapspeed, LEClapspeed, VETlapspeed, RAIlapspeed, GIOlapspeed]
+botspeed = []
+for x in range(len(OAPCircuit[x])):
+    u = random.randint(1,100)
+    if u % 2 == 0:
+        botspeed.append(-1)
+    else:
+        botspeed.append(1)
+    botracers[x].append(OAPCircuit[x] + (botspeed[x] * u))
+
+
 if DwnForce0 == 1:
     for x in range(19):
-        car0lapspeed.append(car0lapspeed[x]+(plusminushigh[x] * random.randint(0, 5)))
+        car0lapspeed.append(car0lapspeed[x] + (plusminushigh[x] * random.randint(0, 5)))
 
 elif DwnForce0 == 3:
     for x in range(19):
         car0lapspeed.append(car0lapspeed[x] + (plusminuslow[x] * random.randint(0, 5)))
 
-# increasing or decreasing speed depending on the level of aro selected by user (car1)
+# increasing or decreasing speed depending on the level of aro selected by user
 if DwnForce1 == 1:
     for x in range(19):
         car1lapspeed.append(car1lapspeed[x] + (plusminushigh[x] * random.randint(0, 5)))
@@ -60,51 +123,15 @@ if DwnForce1 == 1:
 elif DwnForce1 == 3:
     for x in range(19):
         car1lapspeed.append(car1lapspeed[x] + (plusminuslow[x] * random.randint(0, 5)))
-sectorgaptime0 = []
-sectorgaptime1 = []
-for x in range(len(car0lapspeed)):
-    sectorgaptime0.append(0.2615 / (car0lapspeed[x]/3600))
-    sectorgaptime1.append(0.2615 / (car1lapspeed[x]/3600))
-lapnum0 = int(0)
-lapnum1 = int(0)
-time0 = int(0)
-time1 = int(0)
-p = 0
-q = 0
-for x in range(1):
-    for y in range(1):
-        while lapnum0 <= 19:
-            time0 = (time0 + sectorgaptime0[y])
-            lapnum0 = (lapnum0 + 1)
-            print("~~~~~~~~~~~~~~~~~~~~~~~~~~~")
-            print("Car 0")
-            print("speed", car0lapspeed[p])
-            print("time", time0)
-            print("Sector:", p)
-            p = (p + 1)
-            print("~~~~~~~~~~~~~~~~~~~~~~~~~~~")
-            time.sleep(sectorgaptime0[y])
-        print("End of race car0")
-    for z in range(1):
-        while lapnum1 <= 19:
-            time1 = (time1 + sectorgaptime1[z])
-            lapnum1 = (lapnum1 + 1)
-            print("~~~~~~~~~~~~~~~~~~~~~~~~~~~")
-            print("Car 1")
-            print("speed", car1lapspeed[q])
-            print("time", time1)
-            print("Sector:", q)
-            q = (q + 1)
-            print("~~~~~~~~~~~~~~~~~~~~~~~~~~~")
-            time.sleep(sectorgaptime1[z])
-        print("End of race car1")
 
-print("End of race!")
-print("Car0:", time0, "Car1:", time1)
-if time1 > time0:
-    print("Car0 is the winner")
-elif time0 > time1:
-    print("Car1 is the winner")
-else:
-    print("It was a tie!!!!")
+for x in range(len(car1lapspeed)):
+    botracers[x].append(botracers[x] + (u * random.randint(1, 5)))
+
+
+racersectortime = [sectorgaptime0, sectorgaptime1, sectorgaptimeHAM, sectorgaptimeBOT, sectorgaptimeVER, sectorgaptimeALB, sectorgaptimeNOR, sectorgaptimeRIC,  sectorgaptimeOCO, sectorgaptimeSTR, sectorgaptimePER, sectorgaptimeGRO, sectorgaptimeMAG, sectorgaptimeRUS, sectorgaptimeKVY, sectorgaptimePIE, sectorgaptimeLEC, sectorgaptimeVET, sectorgaptimeRAI, sectorgaptimeGIO]
+for x in range(20):
+    for i in range(len(car1lapspeed)):
+        racersectortime[x].append(0.2615 / (racersectortime[i]/3600))
+
+
 
