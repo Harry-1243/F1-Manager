475074292cb617b70b913d6c7b08043457563a39
Harry-1243 <74413940+Harry-1243@users.noreply.github.com>
2021-01-11 12:47:56 +0000

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,5 +1,58 @@
 import random
 import time
+
+def pit0():
+    pitQ = input("do you want to pit car0 next lap")
+    if pitQ == yes:
+        print("What would tyre would you like to change to?")
+        print("1. Soft")
+        print("2. Medium")
+        print("3.Hard")
+        tobtyre0 = input()
+        if tobtyre0 == 1:
+            for x in range(sectorgaptime0):
+                sectorgaptime0.append(sectorgaptime0[x] - 0.6)
+            tyredegrading0 = 2
+            sectorgaptime0.append(sectorgaptime0[x] + random.randint(15, 25))
+
+        elif tobtyre0 == 2:
+            tyredegrading0 = 3
+            sectorgaptime0.append(sectorgaptime0[x] + random.randint(15, 25))
+
+        elif tobtyre0 == 1:
+            for x in range(sectorgaptime0):
+                sectorgaptime0.append(sectorgaptime0[x] + 0.7)
+            tyredegrading0 = 4
+            sectorgaptime0.append(sectorgaptime0[x] + random.randint(15, 25))
+    else:
+        print("Ok")
+
+
+def pit1():
+    pitQ = input("do you want to pit car1 next lap")
+    if pitQ == yes:
+        print("What would tyre would you like to change to?")
+        print("1. Soft")
+        print("2. Medium")
+        print("3.Hard")
+        tobtyre1 = input()
+        if tobtyre1 == 1:
+            for x in range(sectorgaptime1):
+                sectorgaptime1.append(sectorgaptime1[x] - 0.6)
+            tyredegrading1 = 2
+            sectorgaptime1.append(sectorgaptime1[x] + random.randint(15, 25))
+
+        elif tobtyre1 == 2:
+            tyredegrading1 = 3
+            sectorgaptime1.append(sectorgaptime1[x] + random.randint(15, 25))
+        elif tobtyre1 == 1:
+            for x in range(sectorgaptime1):
+                sectorgaptime1.append(sectorgaptime1[x] + 0.7)
+            tyredegrading1 = 4
+            sectorgaptime1.append(sectorgaptime1[x] + random.randint(15, 25))
+    else:
+        print("Ok")
+
 laplength = 6
 car02bpitted = 0
 car12bpitted = 0
@@ -7,6 +60,10 @@ currenttyre1 = ""
 currenttyre0 = ""
 tobtyre1 = ""
 tobtyre0 = ""
+car0crash = 0
+car1crash = 0
+tyredegrading1 = 6
+tyredegrading0 = 6
 car1lapspeed = []
 car0lapspeed = []
 HAMlapspeed = []
@@ -47,12 +104,6 @@ sectorgaptimeLEC = []
 sectorgaptimeVET = []
 sectorgaptimeRAI = []
 sectorgaptimeGIO = []
-
-def pitlane1(Pstime, Tyredelta):
-    Pstime = random.randint(18.9, 21.9)
-    if tobtyre1 == "Medium":
-        
-
 
 print("Please enter settings for car 1")
 
@@ -133,5 +184,44 @@ for x in range(20):
     for i in range(len(car1lapspeed)):
         racersectortime[x].append(0.2615 / (racersectortime[i]/3600))
 
+while laplength > 0:
+    for x in range(len(car1lapspeed)):
+        lapnumber = (lapnumber + 1)
+        totaltime0 = (totaltime0 + sectorgaptime0[x])
+        totaltime1 = (totaltime1 + sectorgaptime1[x])
+        totaltimeHAM = (totaltimeHAM + sectorgaptimeHAM[x])
+        totaltimeBOT = (totaltimeBOT + sectorgaptimeBOT[x])
+        totaltimeVER = (totaltimeVER + sectorgaptimeVER[x])
+        totaltimeALB = (totaltimeALB + sectorgaptimeALB[x])
+        totaltimeNOR = (totaltimeNOR + sectorgaptimeNOR[x])
+        totaltimeRIC = (totaltimeRIC + sectorgaptimeRIC[x])
+        totaltimeOCO = (totaltimeOCO + sectorgaptimeOCO[x])
+        totaltimeSTR = (totaltimeSTR + sectorgaptimeSTR[x])
+        totaltimePER = (totaltimePER + sectorgaptimePER[x])
+        totaltimeGRO = (totaltimeGRO + sectorgaptimeGRO[x])
+        totaltimeMAG = (totaltimeMAG + sectorgaptimeMAG[x])
+        totaltimeRUS = (totaltimeRUS + sectorgaptimeRUS[x])
+        totaltimeKVY = (totaltimeKVY + sectorgaptimeKVY[x])
+        totaltimePIE = (totaltimePIE + sectorgaptimePIE[x])
+        totaltimeLEC = (totaltimeLEC + sectorgaptimeLEC[x])
+        totaltimeVET = (totaltimeVET + sectorgaptimeVET[x])
+        totaltimeRAI = (totaltimeRAI + sectorgaptimeRAI[x])
+        totaltimeGIO = (totaltimeGIO + sectorgaptimeGIO[x])
+        pit0()
+        pit1()
+        tyredegrading()
+        tyredegrading1 = (tyredegrading1 - 1)
+        tyredegrading0 = (tyredegrading0 - 1)
 
 
+def tyredegrading():
+    tyre0per = ((tyredegrading0/lapnumber) * 100)
+    tyre1per = ((tyredegrading1/lapnumber) * 100)
+    print(tyre0per, "%")
+    print(tyre1per, "%")
+    if tyre0per <= 0:
+        print("car0 out of race")
+        car0crash = 1
+    elif tyre1per <= 0:
+        print("car1 out of race")
+        car1crash = 1
